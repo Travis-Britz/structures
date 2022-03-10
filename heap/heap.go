4d279b733cc74ea868d4464d36c66e2b4516b284
Travis Britz <Travis-Britz@users.noreply.github.com>
2022-03-09 21:58:31 -0600

    whitespace

--- heap/heap.go
+++ heap/heap.go
@@ -106,8 +106,5 @@ func Min[T ordered](before T, after T) bool {
 }
 
 type ordered interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
-	~float32 | ~float64 |
-	~string
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~float32 | ~float64 | ~string
 }
